a614d3219d42194bff0d1c71bec778918efcb9d2
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-11-04 12:42:10 +0530

    .

--- scripts/clean_and_rebuild.ps1
+++
@@ -1,5 +0,0 @@
-# Windows PowerShell script to clean and rebuild the project
-cd android
-./gradlew clean
-cd ..
-npx expo start -c
--- scripts/clean_and_rebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Windows PowerShell/Batch version below
-# Remove old build artifacts for a clean build
-cd android
-./gradlew clean
-cd ..
-npx expo start -c
